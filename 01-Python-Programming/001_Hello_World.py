ebcfc9b4cc7b6f00a756b994ffc350b59e7faa6b
Sangamesh M Kuri <sangameshmkuri94@gmail.com>
2026-07-17 16:19:14 +0530

    Updated Hello World Practice

--- 01-Python-Programming/001_Hello_World.py
+++ 01-Python-Programming/001_Hello_World.py
@@ -1,1 +1,11 @@
+print("Hello World!")
 
+FirstName = "SANGAMESH"
+SecondName = "M KURI"
+print(FirstName)
+print(SecondName)
+print(FirstName + " " + SecondName)
+
+Name = input("Enter Your Name : ")
+Age = int(input("Enter Your Age : "))
+print("Hii", Name + " " "Your Age Is : " , Age)
