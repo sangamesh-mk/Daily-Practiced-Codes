f69907d2bcc9b849ced9559762ce2edfd4ea2cb2
Sangamesh M Kuri <sangameshmkuri94@gmail.com>
2026-07-19 11:51:03 +0530

    add

--- 01-Python-Programming/004_Literals_Constants.py
+++ 01-Python-Programming/004_Literals_Constants.py
@@ -1,1 +1,61 @@
+### THERE ARE MAINLY FIVE TYPES OF LITERALS IN PYTHON
+#
+"""
+1.Integer Literals
+2.Float Literals
+3.Boolean Literals
+4.Complex Literals
+5.String Literals 
 
+"""
+##1.Integer Literals
+#Direct integer valus like "200"
+I1 = 12345678910 #Without underscore
+I2 = 123_45_67_8910 #With underscores to enhance the readability
+print(I1) #output: 12345678910
+print(I2) #output: 12345678910
+print(type(I1), type(I2)) #output: <class 'int'> <class 'int'>
+
+"""
+#Invalid integer literals
+I3 = _1234
+print(I3) #output: SyntaxError: invalid token
+
+I4 = 12345_
+print(I4) #output: SyntaxError: invalid token
+
+"""
+
+##2.Float Literals
+#Direct float values like "3.14"
+F1 = 3.14 #Without underscore
+F2 = 3.1_4 #With underscores to enhance the readability
+print(F1)
+print(F2)
+print(type(F1),type(F2))
+
+"""
+#Invalid Float Literals 
+F3 = 54_._8
+F4 = 3_6._8
+print(F3) #output: invalid decimal literal
+print(F4) #output: invalid decimal literal
+"""
+
+##3.Boolean Literals
+#Direct True or False 
+B1 = True
+B2 = False
+print(B1) #output 
+print(B2)
+print(type(B1),type(B2))
+
+##4.Complex Literals
+#Direct Complex Data types assigned to the variables
+C1 = 4+9j
+C2 = 8_5 + 10_3j
+print(C1) #output: 
+print(C2) #output: 
+print(type(C1),type(C2))
+print(C1,C2)
+# Print numbers from 1 to 10
