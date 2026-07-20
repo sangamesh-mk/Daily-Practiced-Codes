8703c25931f5d3057d7bea7d6fce00e60c2409c9
Sangamesh M Kuri <sangameshmkuri94@gmail.com>
2026-07-20 10:17:39 +0530

    ADD THE UPDATED SECTION OF OPERATORS AND EXPRESSION

--- 01-Python-Programming/006_Operators_Expressions.py
+++ 01-Python-Programming/006_Operators_Expressions.py
@@ -1,5 +1,30 @@
-Case3 = ""
-Bool4 = bool(Case3)
-print(Bool4)
-print(type(Case3))
-print(type(Bool4))
+### OPERATORS
+##1. Arithmetic Operators: +,-,*,/,//,%.**
+A = 10
+B = 20
+Add = A + B
+Sub = A - B
+Mult = A * B
+Div = A / B
+FloorD = A // B
+Mod = A % B
+Expon = A ** B
+
+print("Addition : ", Add) #OUTPUT: Addition :  30
+print("Subtraction : ", Sub) #OUTPUT: Subtraction :  -10
+print("Multiplication : ", Mult) #OUTPUT: Multiplication :  200
+print("Division : ", Div) #OUTPUT: Division :  0.5
+print("Floor Division : " ,FloorD) #OUTPUT: Floor Division :  0
+print("Modulus : ", Mod) #OUTPUT: Modulus :  10
+print("Exponent : ", Expon) #OUTPUT: Exponent :  100000000000000000000
+
+### EXPRESSIONS 
+Expr = 2 + 88 * 100 - 9 / 2
+print("The Result is : ", Expr) #OUTPUT: The Result is :  8797.5
+
+Expr1 = (5 + 98) * 20 / 55 
+print("The Total Result : ", Expr1) #OUTPUT: The Total Result :  37.45454545454545
+print(round(Expr1)) #OUTPUT: 37
+
+import math
+print(math.sqrt(Expr1)) #OUTPUT: 6.120011883529758
